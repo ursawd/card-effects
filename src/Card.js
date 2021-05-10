3ddfaf2c243080aeb9d552bc7ab980aff7ecefb0
Phil Bailey <philbailey@msn.com>
2021-05-10 13:05:52 -0400

    edit 2

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,9 @@
 import React from "react";
-const Card = () => {
-  return <p>HEELLOO</p>;
+const Card = ({ image }) => {
+  return (
+    <div>
+      <img src={image} alt="random card" />;
+    </div>
+  );
 };
 export default Card;
--- src/DeckControl.js
+++ src/DeckControl.js
@@ -2,37 +2,27 @@ import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Card from "./Card";
 //---------------------------------------------------------
-let testId;
 const DeckControl = () => {
   const [drawCard, setDrawCard] = useState(false);
   const deckId = useRef("abc");
+  const imageRef = useRef("");
   //---------------------------------------------------------
   //return new shuffled deck id into useRef deckId.current
   useEffect(() => {
-    console.log("first effect");
     async function getNewDeck() {
       let url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
       const res = await axios.get(url);
-      console.log(res.data.deck_id);
-      let temp = res.data.deck_id;
-      deckId.current = temp;
-      testId = temp;
-      console.log("Deck Id: ", deckId.current);
+      deckId.current = res.data.deck_id;
     }
     getNewDeck();
-    return testId;
   }, []);
   //---------------------------------------------------------
   useEffect(() => {
-    console.log("second effect");
     async function getNewCard() {
       let Id = deckId.current;
-      Id = testId;
-      console.log("ID", Id);
       const url = `https://deckofcardsapi.com/api/deck/${Id}/draw/?count=1`;
-      console.log("URL", url);
       const res = await axios.get(url);
-      console.log("RES.DATA", res.data);
+      imageRef.current = res.data.cards[0].image;
     }
     getNewCard();
   }, [drawCard]);
@@ -41,7 +31,7 @@ const DeckControl = () => {
   return (
     <div>
       <button onClick={() => setDrawCard(!drawCard)}>Draw A Card</button>
-      <Card />
+      <Card image={imageRef.current} />
     </div>
   );
 };
